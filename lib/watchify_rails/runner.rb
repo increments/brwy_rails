46da5163102f64f20bbc488307b21309d7ec1272
Koutarou Chikuba <miz404@gmail.com>
2016-04-19 20:51:15 +0900

    Refactor runner

--- lib/watchify_rails/runner.rb
+++ lib/watchify_rails/runner.rb
@@ -6,27 +6,50 @@ module WatchifyRails
       @config = config
     end
 
+    def run_with_watchify(target)
+      ensure_output_dir!
+      spawn watchify_cmd(target), chdir: Rails.root
+    end
+
+    def run_with_browserify(target)
+      ensure_output_dir!
+      spawn browserify_cmd(target), chdir: Rails.root
+    end
+
+    private
+    def browserify_cmd(target)
+      "browserify #{cmd target}"
+    end
+
+    def watchify_cmd(target)
+      "watchify #{cmd target} #{verbose? ? "-v" : ""}"
+    end
+
+    def cmd(target)
+      "#{@config.browserify_opts} -o #{output_path(target)} #{src_path(target)}"
+    end
+
     def output_dir
       Rails.root.join(@config.tmpdir)
     end
 
-    def src_path
-      Rails.root.join(@config.tmpdir)
+    def output_path(target)
+      # TODO: Consider nested dir
+      # Use relative path later
+      output_dir.join(File.basename target)
     end
 
+    def src_path(target)
+      Rails.root.join(target)
+    end
+
+    # TODO: Add ensure_target_dir!(target) later
     def ensure_output_dir!
       FileUtils.mkdir_p(output_dir) unless FileTest.exist?(output_dir)
     end
 
-    def browserify_cmd(target)
-      src_path = Rails.root.join(target)
-      output_path = output_dir.join(File.basename target)
-      "watchify #{@config.browserify_opts} #{if @config.verbose then "-v" else "" end} -o #{output_path} #{src_path}"
-    end
-
-    def run_with_watchify(target)
-      ensure_output_dir!
-      spawn browserify_cmd(target), chdir: Rails.root
+    def verbose?
+      @config.verbose
     end
   end
 end
