d1dd096b24cd8398815fd11c45b2e20e2b39ecfc
Koutarou Chikuba <miz404@gmail.com>
2016-04-20 19:12:03 +0900

    Fix tmpdir option

--- lib/brwy_rails/railtie.rb
+++ lib/brwy_rails/railtie.rb
@@ -3,15 +3,14 @@ module BrwyRails
     # options
     config.brwy_rails = ActiveSupport::OrderedOptions.new
     config.brwy_rails.browserify_opts = ""
-    # TODO: Don't change tmpdir because it can't affect to asset.paths
     config.brwy_rails.tmpdir = "tmp/cache/brwy_rails"
     config.brwy_rails.targets = []
     config.brwy_rails.verbose = true
     config.brwy_rails.target_suffix = ".bundle.js"
     config.brwy_rails.watch = Rails.env.development?
 
-    initializer :assets do |config|
-      Rails.application.config.assets.paths << Rails.root.join("tmp/cache/brwy_rails")
+    initializer :assets do
+      Rails.application.config.assets.paths << Rails.root.join(Rails.application.config.brwy_rails.tmpdir)
     end
 
     config.after_initialize do
